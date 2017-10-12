8eb702e390220b97eb8510d3d4a7e51da33e2770
lhch <lhch102@yeah.net>
2017-10-12 13:40:53 +0800

    test

--- app/src/main/java/com/lhch/ideal/MovieDetailActivity.java
+++ app/src/main/java/com/lhch/ideal/MovieDetailActivity.java
@@ -3,7 +3,6 @@ package com.lhch.ideal;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.IntegerRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
